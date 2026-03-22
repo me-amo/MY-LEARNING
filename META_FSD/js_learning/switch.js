43d1388878cd7a49f0284f7396fb7464b2fa59d9
Emmanuel Amodoi <amodoie@gmail.com>
2026-03-22 08:37:49 +0300

    My switch practice in js

---
+++ META_FSD/js_learning/switch.js
@@ -0,0 +1,26 @@
+var day = "Sunday"
+switch (day){
+    case 'Monday':
+    console.log("Study html")
+    break;
+     case 'Tuesday':
+    console.log("Study css")
+    break;
+     case 'Wednesday':
+    console.log("Study js")
+    break;
+     case 'Thursday':
+    console.log("Study git")
+    break;
+     case 'Friday':
+    console.log("Study databases")
+    break;
+     case 'Saturday':
+    console.log("General Revision")
+    break;
+     case 'Sunday':
+    console.log("Rest")
+    break;
+    default:
+    console.log("There is no such a day")
+}
