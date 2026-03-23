1bb8c5c6e7d1fb96da73bc29434cb78615ae5940
Emmanuel Amodoi <amodoie@gmail.com>
2026-03-23 07:06:14 +0300

    While loop practice

---
+++ META_FSD/js_learning/while.js
@@ -0,0 +1,19 @@
+var i = 1;
+while (i < 6) {
+    console.log(i);
+    i++;
+}
+console.log("Counting completed")
+
+var j=5;
+while (j > 0) {
+    console.log(j);
+    j--;
+}
+console.log("Counting completed")
+
+var year = 2018;
+while (year < 2023) {
+    console.log(year);
+    year++;
+}
